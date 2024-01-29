74f6ed84a7c4b7ec616acd18945fa879a76e7179
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-01-29 16:58:10 +0100

    selection table functioning

--- react-virology-012524/src/components/SelectedSymptoms.jsx
+++ react-virology-012524/src/components/SelectedSymptoms.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
 
-const SelectedSymptoms = () => {
+const SelectedSymptoms = ({symptom, stealth, resistance, stage_speed, transmission, level, effect, required_chemical, threshold, id}) => {
+  var width = "10px";
   return (
-    <div>SelectedSymptoms</div>
+    <>
+      <td className='p-1 font-bold max-w-28 border-[1px] border-dotted border-gray-400'>
+        {symptom}
+      </td>
+
+      <td className={`p-1 w-[${width}] text-center border-[1px] border-dotted border-gray-400`}>
+        {stealth}
+      </td>
+
+      <td className={`p-1 w-[${width}] text-center border-[1px] border-dotted border-gray-400`}>
+        {resistance}
+      </td>
+
+      <td className={`p-1 w-[${width}] text-center border-[1px] border-dotted border-gray-400`}>
+        {stage_speed}
+      </td>
+
+      <td className={`p-1 w-[${width}] text-center border-[1px] border-dotted border-gray-400`}>
+        {transmission}
+      </td>
+
+      <td className={`p-1 w-[${width}] text-center border-[1px] border-dotted border-gray-400`}>
+        {level}
+      </td>
+
+      <td className='p-1 border-[1px] border-dotted max-w-40 border-gray-400'>
+        {effect}
+      </td>
+
+      <td className='p-1 border-[1px] border-dotted border-gray-400'>
+        {required_chemical.map((x, i) => (
+          <ul>
+            <li>
+              -<a className='font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline' 
+              target="_blank" 
+              href={x.link}
+              key={i+"_chemical_"+x.name}
+              >
+                {x.name} 
+              </a>
+            </li>
+          </ul>
+        ))}
+      </td>
+
+      <td className='truncate text-wrap border-[1px] border-dotted border-gray-400'>
+        {threshold.map((x, i) => (
+          <ul>
+            -<span  title={x.title} key={i+"_threshold_"+x.name}>
+              {x.name}
+            </span>
+          </ul>
+        ))}
+      </td>
+    </>
   )
 }
 
--- react-virology-012524/src/components/SymptomsTables.jsx
+++ react-virology-012524/src/components/SymptomsTables.jsx
@@ -1,31 +1,48 @@
 import React, { useState } from 'react'
 import { SYMPTOMS } from '../assets/symptoms'
 import Symptom from './Symptom'
+import SelectedSymptoms from './SelectedSymptoms';
 
 const table_headers = ["Symptom", "Stealth", "Resistance", "Stage Speed", "Transmission", "Level", "Effect", "Required Chemical", "Threshold"];
 
 const SymptomsTables = () => {
 
-  const [isSelected, setSelected] = useState([]);
+  const [isSelected, setIsSelected] = useState([]);
 
-  function handleAddSymptom(symptom) {
-    // check if in selected array
-     
-    // if found, REMOVE from 
-    if (isSelected.includes(symptom.id)) {
-      isSelected.splice(isSelected.indexOf(symptom.id), 1); 
-      symptom.selected = false;
-    } else {
-      isSelected.push(symptom.id);
-      symptom.selected = true;
-    }
-    console.log(symptom);
-    console.log(symptom.selected);
+  function handleClick(e) {
+    this.setIsSelected
   }
 
   return (
     <div className='relative overflow-x-auto'>
       <div className='w-full'>
+
+        {/* SELECTED SYMPTOMS TABLE */}
+
+        <table className='sm:m-10 w-screen'>
+          <thead>
+            <tr className='font-extralight text-xs text-white bg-slate-800'>
+              {table_headers.map((header, i) => <th className='break-all' key={i+"_header"}>{header}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            {isSelected.map((symptom) => (
+              <tr className={`text-xs ${symptom.selected ? "bg-blue-300" : "even:bg-gray-50 odd:bg-white"}
+              hover:bg-slate-900 hover:text-white duration-300 `} 
+                  key={symptom.id+"_symptom_row"}
+                  onClick={() => {
+                    symptom.selected = false;
+                    setIsSelected(isSelected.filter(a => a.id !== symptom.id));}
+                  }
+                  >
+                <SelectedSymptoms key={symptom.id} {...symptom} />
+              </tr>))}
+          </tbody>
+        </table>
+
+        </div>
+        {/* ALL SYMPTOMS TABLE */}
+        <div>
         <table className='sm:m-10'>
           <thead>
             <tr className='font-extralight text-xs text-white bg-slate-800'>
@@ -37,9 +54,17 @@ const SymptomsTables = () => {
               <tr className={`text-xs ${symptom.selected ? "bg-blue-300" : "even:bg-gray-50 odd:bg-white"}
               hover:bg-slate-900 hover:text-white duration-300 `} 
                   key={symptom.id+"_symptom_row"}
-                  onClick={() => handleAddSymptom(symptom)}
+                  onClick={() => {
+                    if (symptom.selected) {
+                      symptom.selected = false;
+                      setIsSelected(isSelected.filter(a => a.id !== a.id));
+                    } else {
+                      symptom.selected = true;
+                      setIsSelected([...isSelected, symptom]);
+                    }
+                  }}
                   >
-                <Symptom key={symptom.id} {...symptom} value={symptom.id} />
+                <Symptom key={symptom.id} {...symptom} />
               </tr>))}
           </tbody>
         </table>
