a19acee4f899ced42d1885e8210feea3b910b5f0
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-02-17 14:02:01 +0100

    threshold data formatted

--- react-virology-012524/src/components/ScoreChart.jsx
+++ react-virology-012524/src/components/ScoreChart.jsx
@@ -38,44 +38,66 @@ const ScoreChart = ({stealth_s, resistance_s, stage_speed_s, transmission_s, thr
     },
   ];
 
+  thresholds.map((t, i) => {
+    switch(t.name) {
+      case "Stealth":
+        data[0][`value${i}`] = t.value;
+        data[0][`title${i}`] = t.title;
+        break;
+      case "Resistance":
+        data[1][`value${i}`] = t.value;
+        data[1][`title${i}`] = t.title;
+        break;
+      case "Stage Speed":
+        data[2][`value${i}`] = t.value;
+        data[2][`title${i}`] = t.title;
+        break;
+      case "Transmission":
+        data[3][`value${i}`] = t.value;
+        data[3][`title${i}`] = t.title;
+        break;
+      default:
+        break;
+    }
+  });
+
+  console.log("We looked at the data...", data);
+
   return (
-    <div className='w-full h-full'>
-      THRESHES
-      {thresholds.map((t) => {
-        console.log(t.name);
-      })}
-    </div>
+  <ResponsiveContainer width="100%" height="100%">
+    <ComposedChart
+      width={1000}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="1 1" />
+      <XAxis dataKey="symptom" />
+      <YAxis />
+      <Tooltip />
+      <ReferenceLine y={0} stroke="#000" />
+      <Bar dataKey="Value" fill="#82ca9d" />
+      <Scatter dataKey="value0" fill="red" />
+      <Scatter dataKey="value1" fill="red" />
+      <Scatter dataKey="value2" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+      <Scatter dataKey="value3" fill="red" />
+    </ComposedChart>
+  </ResponsiveContainer>
   )
 }
 
 export default ScoreChart;
-
-    // <ResponsiveContainer width="100%" height="100%">
-    //   <ComposedChart
-    //     width={1000}
-    //     height={300}
-    //     data={data}
-    //     margin={{
-    //       top: 5,
-    //       right: 30,
-    //       left: 20,
-    //       bottom: 5,
-    //     }}
-    //   >
-    //     <CartesianGrid strokeDasharray="1 1" />
-    //     <XAxis dataKey="symptom" />
-    //     <YAxis />
-    //     <Tooltip />
-
-    //     <Bar dataKey="Value" fill="#27ae60" />
-
-    //     <ReferenceLine y={0} stroke="#000" />
-
-    //     <Scatter dataKey="threshold1" fill="red" />
-    //     <Scatter dataKey="threshold1" fill="red" />
-    //     <Scatter dataKey="threshold1" fill="red" />
-    //     <Scatter dataKey="threshold1" fill="red" />
-    //     <Bar dataKey="uv" fill="#82ca9d" />
-
-    //   </ComposedChart>
-    // </ResponsiveContainer>
