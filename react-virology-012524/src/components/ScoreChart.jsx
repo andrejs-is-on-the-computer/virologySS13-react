94f2934168b547c48a842f49fa482c1d36683e77
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-02-20 13:35:39 +0100

    data array needs to be 0-index

--- react-virology-012524/src/components/ScoreChart.jsx
+++ react-virology-012524/src/components/ScoreChart.jsx
@@ -17,58 +17,48 @@ import {
 
 const ScoreChart = ({stealth_s, resistance_s, stage_speed_s, transmission_s, thresholds}) => {
 
-  // console.log("Thresholds", thresholds);
-
   const data = [
     {
       stat: "Stealth",
-      Value: stealth_s,
+      value: stealth_s,
       tresh: []
     },
     {
       stat: "Resistance",
-      Value: resistance_s,
+      value: resistance_s,
       tresh: []
     },
     {
       stat: "Stage Speed",
-      Value: stage_speed_s,
+      value: stage_speed_s,
       tresh: []
     },
     {
       stat: "Transmission",
-      Value: transmission_s,
+      value: transmission_s,
       tresh: []
     },
   ];
 
   thresholds.map((t, i) => {
-    console.log('maaaappoping',t);
     switch(t.name) {
       case "Stealth":
         data[0].tresh.push(t);
         break;
       case "Resistance":
-        data[0].tresh.push(t);
+        data[1].tresh.push(t);
         break;
       case "Stage Speed":
-        data[0].tresh.push(t);
+        data[2].tresh.push(t);
         break;
       case "Transmission":
-        data[0].tresh.push(t);
+        data[3].tresh.push(t);
         break;
       default:
         break;
     }
   });
-  // Return
-  // 1. Stat name (Stealth, Transmission, etc.) %
-  // 2. Current value of stat %
-  // 3. Thresholds 
-  //    3-1. Threshold Symptom %
-  //    3-2. Threshold Value %
   const getThreshes = (threshes) => {
-    console.log("Chart",threshes);
     if (threshes.length > 0){
       return threshes.map((t) => {
         return <>
@@ -83,11 +73,10 @@ const ScoreChart = ({stealth_s, resistance_s, stage_speed_s, transmission_s, thr
 
   const CustomToolTip = ({ active, payload }) => {
     if (active && payload && payload.length) {
-      console.log("PAYLOAD", payload[0].payload);
       return (
-        <div>
-          <p>Stat: {payload[0].payload.stat}</p>
-          <p>Value: {payload[0].payload.value}</p>
+        <div className='bg-white p-4'>
+          <p><b>{payload[0].payload.stat}</b></p>
+          <p><b>Value:</b> {payload[0].payload.value}</p>
           <b>Thresholds:</b>
           <ul>
             {
@@ -118,7 +107,10 @@ const ScoreChart = ({stealth_s, resistance_s, stage_speed_s, transmission_s, thr
       <YAxis />
       <Tooltip content={<CustomToolTip />} />
       <ReferenceLine y={0} stroke="#000" />
-      <Bar dataKey="Value" fill="#82ca9d" />
+      <Bar dataKey="value" fill="#82ca9d" />
+      {data.tresh.map((t) => (
+        <Scatter dataKey="t.value" />
+      ))}
       {/* <Scatter dataKey="value0" fill="red" /> */}
     </ComposedChart>
   </ResponsiveContainer>
